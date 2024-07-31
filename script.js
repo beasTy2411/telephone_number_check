451ffd9425c0c76255a190a752ff9f5842e43746
beasty2411 <navis.kumar1095@gmail.com>
2024-07-31 16:14:52 +0530

    Finished the working for US numbers

--- script.js
+++ script.js
@@ -2,10 +2,36 @@ const input = document.getElementById("user-input");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const resultDiv = document.getElementById("results-div");
+let outputMSG = "";
 
-const checkNum = () =>{
-  const regex = /([^0-9()-])/g;
-  let outputMSG="";
+
+function validator(){
+const inputVal = input.value;
+const regex1 = /(^(1\s?)?)((\([0-9]{3}\)|[0-9]{3}))([\s\-]?)([0-9]{3})([\s\-]?)([0-9]{4}$)/g;
+
+if(!regex1.test(inputVal)){
+  outputMSG =`Invalid US number: ${inputVal}`;
+}else{
+  const regmain = /[\s\(\)-]/g;
+  let modInput = inputVal.replace(regmain,"");
+  console.log(modInput);
+
+  if(modInput.length == 11){
+    if(modInput[0]==="1"){
+      input.value = ""
+      outputMSG = `Valid US number: ${inputVal}`;
+    }else{
+      input.value = ""
+      outputMSG = `Invalid US number: ${inputVal}`;
+    }
+  }else if(modInput.length==10){
+    input.value = ""
+    outputMSG = `Valid US number: ${inputVal}`;
+  }else{
+    input.value = ""
+    outputMSG = `Invalid US number: ${inputVal}`;
+  }
+}
 }
 
 function checkFunc(){
@@ -22,6 +48,7 @@ const clearFunc=()=>{
 }
 
 const updateUI=()=>{
+  validator();
   resultDiv.innerText = outputMSG;
   resultDiv.classList.remove("hidden");
 }
